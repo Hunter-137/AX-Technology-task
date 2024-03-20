d42ea79c577d55228762937cdb1c7394f3cce8ef
Hunter-137 <asrarkulovamal@gmail.com>
2024-03-20 22:55:39 +0500

    feat: add modal window for brochure

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+// запуск DOM
+document.addEventListener("DOMContentLoaded", function () {
+  // ************************************************************************************************************** Модальное окно
+  const modalWindow = document.querySelector(".modal"); // модальное окно
+  const modalOpenBtn = document.querySelector(".footer-download"); // кнопка открытия модального окна
+  const modalCloseBtn = document.querySelector(".modal__close-btn"); // кнопка крестик — закрытие модального окна
+
+  // открытие модального окна по клику на кнопку
+  modalOpenBtn.addEventListener("click", function () {
+    modalWindow.classList.add("_active");
+  });
+
+  // закрытие модального окна по клику на крестик
+  modalCloseBtn.addEventListener("click", function () {
+    modalWindow.classList.remove("_active");
+  });
+});
